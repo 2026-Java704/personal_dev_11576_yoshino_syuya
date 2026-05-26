dd5aa1092c35fd0f1797d85384a0cbbfc086cda0
student <student@FJ-1025>
2026-05-26 11:18:31 +0900

    完成

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,8 +1,5 @@
 package com.example.demo.controller;
 
-
-
-
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -51,34 +48,34 @@ public class HomeController {
 
 		List<Expens> expensList = expensRepository.findAll();
 		List<Category> categoryList = categoryRepository.findAll();
-		
+
 		model.addAttribute("expens", expensList);
 		model.addAttribute("category", categoryList);
-		
+
 		List<Object[]> categorySum = expensRepository.sumPriceByCategory();
 		Integer totalSum = expensRepository.findAll()
-		.stream()
-		.mapToInt(Expens::getPrice)
-		.sum();
+				.stream()
+				.mapToInt(Expens::getPrice)
+				.sum();
 		model.addAttribute("categorySum", categorySum);
 		model.addAttribute("totalSum", totalSum);
-		
+
 		List<Integer> sumprice = expensRepository.sumExpens();
 		List<String> categoryname = expensRepository.categoryname();
-		
+
 		List<String> label = new ArrayList<>();
 		List<Integer> data = new ArrayList<Integer>();
-		
+
 		label.addAll(categoryname);
 		data.addAll(sumprice);
-		
+
 		model.addAttribute("label", label);
 		model.addAttribute("data", data);
-		
+
 		Integer keisan = account.getIncome() - totalSum;
 		model.addAttribute("keisan", keisan);
-		
-		if(keisan < 0) {
+
+		if (keisan < 0) {
 			model.addAttribute("yabai");
 		}
 
@@ -89,7 +86,7 @@ public class HomeController {
 	public String addExpens(Model model) {
 		List<Category> categoryList = categoryRepository.findAll();
 		model.addAttribute("categories", categoryList);
-		
+
 		return "addExpens";
 	}
 
@@ -100,33 +97,33 @@ public class HomeController {
 			@RequestParam(defaultValue = "") Integer price1,
 			@RequestParam(defaultValue = "") String name,
 			Model model) {
-		
+
 		List<Category> categoryList = categoryRepository.findAll();
 		model.addAttribute("categories", categoryList);
-		
+
 		List<String> errorList = new ArrayList<>();
-		
+
 		if (category == null) {
 			errorList.add("カテゴリが選択されていません");
 		}
-		
+
 		if (date == null) {
 			errorList.add("日付が選択されていません");
 		}
-		
+
 		if (price1 == null) {
 			errorList.add("金額が入力されていません");
 		}
-		
+
 		if (name == null || name.length() == 0) {
 			errorList.add("項目名が入力されていません");
 		}
-		
+
 		if (errorList.size() > 0) {
-			model.addAttribute("errorList",errorList);
+			model.addAttribute("errorList", errorList);
 			return "addExpens";
 		}
-		
+
 		Integer price = price1 * 100;
 
 		Expens expens = new Expens(category, date, price, name);
@@ -135,27 +132,27 @@ public class HomeController {
 
 		return "redirect:/home";
 	}
-	
+
 	@PostMapping("/expens/{id}/delete")
 	public String deleteExpens(@PathVariable Integer id) {
-		
+
 		expensRepository.deleteById(id);
-		
+
 		return "redirect:/home";
 	}
-	
+
 	@GetMapping("/expens/{id}/edit")
 	public String edit(@PathVariable Integer id, Model model) {
-		
+
 		List<Category> categoryList = categoryRepository.findAll();
 		model.addAttribute("categories", categoryList);
-		
+
 		Expens expens = expensRepository.findById(id).get();
-		model.addAttribute("expens",expens);
-		
+		model.addAttribute("expens", expens);
+
 		return "editExpens";
 	}
-	
+
 	@PostMapping("/expens/{id}/edit")
 	public String update(
 			@PathVariable Integer id,
@@ -164,63 +161,63 @@ public class HomeController {
 			@RequestParam(defaultValue = "") Integer price1,
 			@RequestParam(defaultValue = "") String name,
 			Model model) {
-		
+
 		List<Category> categoryList = categoryRepository.findAll();
 		model.addAttribute("categories", categoryList);
-		
+
 		List<String> errorList = new ArrayList<>();
-		
+
 		if (category == null) {
 			errorList.add("カテゴリが選択されていません");
 		}
-		
+
 		if (date == null) {
 			errorList.add("日付が選択されていません");
 		}
-		
+
 		if (price1 == null) {
 			errorList.add("金額が入力されていません");
 		}
-		
+
 		if (name == null || name.length() == 0) {
 			errorList.add("項目名が入力されていません");
 		}
-		
+
 		if (errorList.size() > 0) {
-			model.addAttribute("errorList",errorList);
+			model.addAttribute("errorList", errorList);
 			return "addExpens";
 		}
-		
+
 		Integer price = price1 * 100;
-		
+
 		Expens expens = expensRepository.findById(id).get();
 		expens.setCategory(category);
 		expens.setDate(date);
 		expens.setPrice(price);
 		expens.setName(name);
-		
+
 		expensRepository.save(expens);
-		
+
 		return "redirect:/home";
 	}
-	
+
 	@GetMapping("/category/add")
 	public String addCategory(Model model) {
-		
+
 		List<Category> categoryList = categoryRepository.findAll();
 		model.addAttribute("categories", categoryList);
-		
+
 		return "addCategory";
 	}
-	
+
 	@PostMapping("/category/add")
 	public String addedExpens(
 			@RequestParam(defaultValue = "") String name,
 			Model model) {
-		
+
 		List<Category> categoryList = categoryRepository.findAll();
 		model.addAttribute("categories", categoryList);
-		
+
 		if (name == null || name.length() == 0) {
 			model.addAttribute("message", "入力してください");
 			return "addCategory";
@@ -232,89 +229,89 @@ public class HomeController {
 
 		return "redirect:/category/add";
 	}
-	
+
 	@PostMapping("/category/{id}/delete")
 	public String deleteCategory(@PathVariable Integer id) {
-		
+
 		categoryRepository.deleteById(id);
-		
+
 		return "redirect:/category/add";
 	}
-	
-	@GetMapping({"/","/logout"})
+
+	@GetMapping({ "/", "/logout" })
 	public String login() {
 		session.invalidate();
 		return "login";
 	}
-	
+
 	@PostMapping("/login")
 	public String login(
 			@RequestParam(defaultValue = "") String name,
 			Model model) {
-		
+
 		List<User> username = userRepository.findByName(name);
-		
+
 		if (name == null || name.length() == 0) {
 			model.addAttribute("message", "名前を入力してください");
 			return "login";
 		}
-		
-        if (username == null || username.size() == 0) {
+
+		if (username == null || username.size() == 0) {
 			model.addAttribute("message", "名前が存在しません");
 			return "login";
-        }
-        
-        List<Expens> expensList = expensRepository.findAll();
+		}
+
+		List<Expens> expensList = expensRepository.findAll();
 		List<Category> categoryList = categoryRepository.findAll();
-		
+
 		model.addAttribute("expens", expensList);
 		model.addAttribute("category", categoryList);
-		
+
 		List<Object[]> categorySum = expensRepository.sumPriceByCategory();
 		Integer totalSum = expensRepository.findAll()
-		.stream()
-		.mapToInt(Expens::getPrice)
-		.sum();
+				.stream()
+				.mapToInt(Expens::getPrice)
+				.sum();
 		model.addAttribute("categorySum", categorySum);
 		model.addAttribute("totalSum", totalSum);
-		
+
 		List<Integer> sumprice = expensRepository.sumExpens();
 		List<String> categoryname = expensRepository.categoryname();
-		
+
 		List<String> label = new ArrayList<>();
 		List<Integer> data = new ArrayList<Integer>();
-		
+
 		label.addAll(categoryname);
 		data.addAll(sumprice);
-		
+
 		model.addAttribute("label", label);
 		model.addAttribute("data", data);
-		
+
 		Integer income = username.get(0).getIncome();
-		
+
 		account.setName(name);
 		account.setIncome(income);
-		
+
 		Integer keisan = account.getIncome() - totalSum;
 		model.addAttribute("keisan", keisan);
-		
+
 		return "home";
 	}
-	
+
 	@GetMapping("/user")
 	public String addUser() {
 		return "user";
 	}
-	
+
 	@PostMapping("/user")
 	public String logined(
 			@RequestParam(defaultValue = "") String name,
 			@RequestParam(defaultValue = "") Integer income) {
-		
+
 		User user = new User(name, income);
 
 		userRepository.save(user);
-		
+
 		return "login";
 	}
 
